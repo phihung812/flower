655ed0584d6654d725714b2790b7ef1ae3b077db
phihung812 <hungbpph49994@gmail.com>
2024-11-15 09:21:04 +0700

    Update productController.php

--- controller/productController.php
+++ controller/productController.php
@@ -1,7 +1,7 @@
 <?php
 include_once(__DIR__ . '/../model/product.php');
 include_once(__DIR__ . '/../model/danhmuc.php');
-include_once(__DIR__ . '/../model/binhluan.php');
+
 class ProductController
 {
     public function addProduct()
