9ae09910d945fce08a1bc8240a01dbc2fb65d63c
Bùi Phi Hùng <hungbui@example.com>
2024-12-06 18:39:01 +0700

    updaTE

--- controller/ProductStatisticController.php
+++ controller/ProductStatisticController.php
@@ -1,16 +1,24 @@
 <?php
 
 include_once(__DIR__ . '/../model/ProductStatisticModel.php');
-class ProductStatisticController {
+class ProductStatisticController
+{
     private $model;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->model = new ProductStatisticModel();
+
     }
 
-    public function getStatistics() {
+    public function getStatistics()
+    {
         $statistics = $this->model->getProductStatistics();
-        
+        $danhmuc = $this->model->all_danhmuc();
+        $sanpham = $this->model->all_product();
+        $donhang = $this->model->all_oder();
+        $bienthe = $this->model->all_bienthe();
+        $chitietdonhang = $this->model->all_odreitem();
         require_once __DIR__ . "/../view/admin/thongke/ProductStatisticView.php";
     }
 }
